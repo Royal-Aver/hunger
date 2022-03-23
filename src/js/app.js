03b29d5cc955523b4ec68e4e68b238ed75593e0b
Royal-Aver <royal_aver@rambler.ru>
2022-03-23 06:55:42 +0300

    continue

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+import * as flsFunction from "./modules/function.js";
+
+flsFunction.isWebp();
+
+new Swiper('.swiper', {
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: 'true',
+  },
+})
